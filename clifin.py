650174096dac5ba2a74d69ab1dd05ce195b530a8
Lochana Ranatunga <161409757+lochanalmr@users.noreply.github.com>
2026-07-10 15:45:13 +0530

    Update clifin.py
    Edited menu entries, to accommodate new functionality.

--- clifin.py
+++ clifin.py
@@ -1,6 +1,6 @@
-from shared import VERSION
+from shared import VERSION, safe_input
 from greeting import ensure_and_greet_user
-from transactions import data_entry, data_read
+from transactions import data_entry, data_read, manage_transaction
 from assets import add_asset_entry, update_asset_value
 from financial_status import view_current_financial_status
 from help_module import help
@@ -9,17 +9,19 @@ from help_module import help
 if __name__ == '__main__':
     ensure_and_greet_user()
     print(f"You are currently running CliFin v{VERSION}")
+    
     while True:
         print("\nMain Menu")
         print("1. Create New Transaction Record 💵")
         print("2. Create New Asset Record 💰")
         print("3. Update Value of Existing Asset 💹")
-        print("4. View Historical Data 🕒")
-        print("5. View Financial Status 🗽")
-        print("6. Get Me Out of Here! 🚪")
+        print("4. Manage Existing Transactions 📝")
+        print("5. View Historical Data 🕒")
+        print("6. View Financial Status 🗽")
         print("7. Explain Me! 😣")
+        print("8. Get Me Out of Here! 🚪")
 
-        choice = input("Choose an option (1, 2, 3, 4, 5, 6, or 7): ").strip()
+        choice = safe_input("Choose an option (1, 2, 3, 4, 5, 6, 7, or 8): ").strip()
 
         if choice == '1':
             data_entry()
@@ -28,15 +30,17 @@ if __name__ == '__main__':
         elif choice == '3':
             update_asset_value()
         elif choice == '4':
-            data_read()
+            manage_transaction()
         elif choice == '5':
+            data_read()
+        elif choice == '6':
             view_current_financial_status()
         elif choice == '7':
             help()
-        elif choice == '6':
+        elif choice == '8':
             print("Thank you for using CliFin!😎")
-            input("Press [Enter] to exit.")
+            safe_input("Press [Enter] to exit.")
             break
         else:
-            print("Invalid choice. Please enter 1, 2, 3, 4, 5, or 6.")
+            print("Invalid choice. Please enter 1, 2, 3, 4, 5, 6, 7, or 8.")
 
